e04fcf978f4a30f1ff853822232363ed7bac4ff4
mimansaup <mimansaupreti28@gmail.com>
2025-10-25 18:21:40 +0530

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,23 @@
+CREATE DATABASE IF NOT EXISTS orderlytics;
+USE orderlytics;
 
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    Country VARCHAR(50)
+);
+
+CREATE TABLE Products (
+    StockCode VARCHAR(20) PRIMARY KEY,
+    Description VARCHAR(255),
+    UnitPrice DECIMAL(10,2)
+);
+
+CREATE TABLE Sales (
+    InvoiceNo VARCHAR(20),
+    InvoiceDate DATETIME,
+    StockCode VARCHAR(20),
+    Quantity INT,
+    CustomerID INT,
+    FOREIGN KEY (StockCode) REFERENCES Products(StockCode),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
